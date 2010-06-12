f11293ad1afc13da8137a2d08b74bcb45520d013
System Administrator <andrew.cove@gmail.com>
2010-06-11 21:18:33 -0700

    Pointing twitter urls at api.twitter.com

--- lib/authlogic_connect/oauth/tokens/twitter_token.rb
+++ lib/authlogic_connect/oauth/tokens/twitter_token.rb
@@ -2,7 +2,7 @@ class TwitterToken < OauthToken
   
   key :user_id
   
-  settings "http://twitter.com",
-    :authorize_url => "http://twitter.com/oauth/authenticate"
+  settings "http://api.twitter.com",
+    :authorize_url => "http://api.twitter.com/oauth/authenticate"
   
 end
