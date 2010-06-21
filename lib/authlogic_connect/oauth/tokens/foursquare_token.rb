7485ce440a2468a8d81090fe1e2b426fa3a696a8
System Administrator <andrew.cove@gmail.com>
2010-06-20 19:32:00 -0700

    Adding key to fsq token.

--- lib/authlogic_connect/oauth/tokens/foursquare_token.rb
+++ lib/authlogic_connect/oauth/tokens/foursquare_token.rb
@@ -1,6 +1,14 @@
 class FoursquareToken < OauthToken
   
-  key :user_id
+  key do |access_token|
+    if access_token.consumer.http and access_token.consumer.http.address == "foursquare.com"    
+      # reset the consumer
+      access_token.consumer=access_token.consumer.class.new(credentials[:key], credentials[:secret], config.merge(credentials[:options] || {}))
+    end
+
+    body = JSON.parse(access_token.get("/v1/user.json").body)
+    user_id = body['user']['id'].to_s
+  end
   
   settings "http://api.foursquare.com",
     :request_token_url => "http://foursquare.com/oauth/request_token",
