f32a63cb0b1ee833e25b96dba9a61e6024edf162
Jonathan Spooner <jspooner@md00030009.active.local>
2010-10-25 12:01:22 -0700

    Added https to twitter

--- lib/authlogic_connect/oauth/tokens/twitter_token.rb
+++ lib/authlogic_connect/oauth/tokens/twitter_token.rb
@@ -2,7 +2,7 @@ class TwitterToken < OauthToken
   
   key :user_id
   
-  settings "http://api.twitter.com",
+  settings "https://api.twitter.com",
     :authorize_url => "http://api.twitter.com/oauth/authenticate"
   
 end
