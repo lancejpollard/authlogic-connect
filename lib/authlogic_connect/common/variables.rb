0851cf56c8a4792d4244c478b7478aabae4394b1
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2010-11-19 11:00:32 -0200

    Do not change controller params/sessions hash keys
    When symbolizing session keys, the Rails flash hash stops working,
    besides other possible issues that might happen.

--- lib/authlogic_connect/common/variables.rb
+++ lib/authlogic_connect/common/variables.rb
@@ -2,42 +2,32 @@ module AuthlogicConnect::Common::Variables
   include AuthlogicConnect::Common::State
 
   attr_reader :processing_authentication
-  
+
   def auth_class
     is_auth_session? ? self.class : session_class
   end
-  
+
   def auth_controller
     is_auth_session? ? controller : session_class.controller
   end
-  
+
   def auth_params
-    return nil unless auth_controller?
-    auth_controller.params.symbolize_keys!
-    auth_controller.params.keys.each do |key|
-      auth_controller.params[key.to_s] = auth_controller.params.delete(key) if key.to_s =~ /^OpenID/
-    end
-    auth_controller.params
+    convert_open_id_keys_from_hash(auth_controller.params) if auth_controller?
   end
-  
+
   def auth_session
-    return nil unless auth_controller?
-    auth_controller.session.symbolize_keys!
-    auth_controller.session.keys.each do |key|
-      auth_controller.session[key.to_s] = auth_controller.session.delete(key) if key.to_s =~ /^OpenID/
-    end
-    auth_controller.session
+    convert_open_id_keys_from_hash(auth_controller.session) if auth_controller?
   end
-  
+
   def auth_callback_url(options = {})
     auth_controller.url_for({:controller => auth_controller.controller_name, :action => auth_controller.action_name}.merge(options))
   end
-  
+
   # if we've said it's a "user" (registration), or a "session" (login)
   def auth_type
     from_session_or_params(:authentication_type)
   end
-  
+
   # auth_params and auth_session attributes are all String!
   def from_session_or_params(attribute)
     return nil unless auth_controller?
@@ -46,16 +36,16 @@ module AuthlogicConnect::Common::Variables
     result = auth_session[key] if (result.nil? || result.blank?)
     result
   end
-  
+
   def add_session_key(key, value)
-    
+
   end
-  
+
   def remove_session_key(key)
     keys = key.is_a?(Symbol) ? [key, key.to_s] : [key, key.to_sym]
     keys.each {|k| auth_session.delete(k)}
   end
-    
+
   # wraps the call to "save" (in yield).
   # reason being, we need to somehow not allow oauth/openid validations to run
   # when we don't have a block.  We can't know that using class methods, so we create
@@ -67,14 +57,14 @@ module AuthlogicConnect::Common::Variables
     @processing_authentication = false
     saved
   end
-  
+
   # returns boolean
   def authentication_protocol(with, phase)
     returning(send("#{phase.to_s}_#{with.to_s}?")) do |ready|
       send("#{phase.to_s}_#{with.to_s}") if ready
     end if send("using_#{with.to_s}?")
   end
-  
+
   # it only reaches this point once it has returned, or you
   # have manually skipped the redirect and save was called directly.
   def cleanup_authentication_session(options = {}, &block)
@@ -84,13 +74,13 @@ module AuthlogicConnect::Common::Variables
       end
     end
   end
-  
+
   def log(*methods)
     methods.each do |method|
       puts "#{method.to_s}: #{send(method).inspect}"
     end
   end
-    
+
   def log_state
     log(:correct_request_class?)
     log(:using_oauth?, :start_oauth?, :complete_oauth?)
@@ -99,7 +89,7 @@ module AuthlogicConnect::Common::Variables
     log(:authenticating_with_openid?)
     log(:stored_oauth_token_and_secret)
   end
-  
+
   # because we may need to store 6+ session variables, all with pretty lengthy names,
   # might as well just tinify them.
   # just an idea
@@ -116,9 +106,16 @@ module AuthlogicConnect::Common::Variables
     }
     @optimized_session_keys[key]
   end
-  
+
   def auto_register?
     true
   end
-  
+
+private
+
+  def convert_open_id_keys_from_hash(hash)
+    hash.each_key do |key|
+      hash[key.to_s] = hash.delete(key) if key.to_s =~ /^OpenID/
+    end
+  end
 end
